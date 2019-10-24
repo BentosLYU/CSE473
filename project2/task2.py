be0e6e0e4c1073cfe31dea6500c49477d15f9b39
DillyPickly <dylan.sabuda@gmail.com>
2019-10-23 20:44:32 -0400

    project2 complete

--- project2/task2.py
+++ project2/task2.py
@@ -33,7 +33,7 @@ def solution(left_img, right_img):
 
 
 
-    # 1: Use Kaze to fiind the features
+    # 1: Use Kaze to find the features
     kaze = cv2.KAZE_create(threshold=20)
     right_keypoints, right_desc = kaze.detectAndCompute(right_gray, None)
     left_keypoints, left_desc = kaze.detectAndCompute(left_gray, None)
@@ -60,7 +60,7 @@ def solution(left_img, right_img):
 
     if print_results:
         # draw all matches
-        res = np.empty((max(left_img.shape[0], right_img.shape[0]), left_img.shape[1] + right_img.shape[1], 3), dtype=np.uint8)
+        res = np.empty((left_img.shape[0]+right_img.shape[0], left_img.shape[1] + right_img.shape[1], 3), dtype=np.uint8)
         res = cv2.drawMatches(left_img, left_keypoints, right_img, right_keypoints, matches, res,
                               flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 
@@ -68,18 +68,40 @@ def solution(left_img, right_img):
 
     # 3: Compute the homeography matrix using RANSAC
     left_pts = np.float32([left_keypoints[m.queryIdx].pt for m in matches])
-    right_pts = np.float32([right_keypoints[m.trainIdx].pt for m in matches])#.reshape(-1, 1, 2)
+    right_pts = np.float32([right_keypoints[m.trainIdx].pt for m in matches])
     H, _ = cv2.findHomography(right_pts, left_pts, cv2.RANSAC, 5.0)
     # Gives a transformation from the right coords to the left coords
 
+    # find bounds of warped image
+    # this is to make sure that the right image is not sent to negative coordinates in the left image
+    xhigh = right_img.shape[1]
+    yhigh = right_img.shape[0]
+    corners = np.array([[0, xhigh,      0,  xhigh],
+                        [0,     0,  yhigh,  yhigh],
+                        [1,     1,      1,      1]])
+    new_corners = np.matmul(H, corners)
+    new_corners /=new_corners[2, :]
+    new_yhigh = int(max(new_corners[1, :]))
+    offset = 0
+    if min(new_corners[1, :]) < 0:
+        offset = int(-min(new_corners[1, :]))
+        new_yhigh = new_yhigh + offset
+        print(offset)
+
+    T = np.array([[1, 0, 0],
+                  [0, 1, offset],
+                  [0, 0, 1]])
+    H = np.matmul(T,H)
+
     # 4: Use the homographic matrix to stitch the images together
-    dims = (right_img.shape[1]+left_img.shape[1], right_img.shape[0])
+    dims = (right_img.shape[1]+left_img.shape[1], max(new_yhigh, left_img.shape[0]))
     result = cv2.warpPerspective(right_img, H, dims) # warp the right image into left image coords
 
     if print_results:
        cv2.imshow('transformed image', result)
 
-    result[0:left_img.shape[0], 0:left_img.shape[1]] = left_img
+    result[offset:offset+left_img.shape[0], 0:left_img.shape[1]] = left_img
+
 
     if print_results:
         cv2.imshow('result', result)
