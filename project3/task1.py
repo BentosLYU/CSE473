37b04c9d1f69908553330ba2d0c544e61b42fdd2
DillyPickly <dylan.sabuda@gmail.com>
2019-11-19 18:38:01 -0500

    Single Iteration of k-means complete

--- project3/task1.py
+++ project3/task1.py
@@ -32,24 +32,70 @@ def kmeans(img,k):
             Clustering labels of all pixels;
             Minimum summation of distance between each pixel and its center.  
     """
+    # Return Values
+    # best_center_pts = np.random.choice(np.arange(256), 2, replace=False)
+    # best_img_labels = np.empty_like(img)
+    # min_error = 99999
 
-    best_pt = np.random.choice(np.arange(256), 2, replace=False)
-    best_img_labels = np.zeros_like(img)
-    min_error = int(9999999999)
 
     # Randomly choose k-centers
     # Initialize centers in range [0,255]
-    init_pts = np.random.choice(np.arange(256), 2, replace=False)
+    rows, columns = img.shape
+    x_s = np.random.choice(rows, k, replace=True)
+    y_s = np.random.choice(columns, k, replace=True)
+    # print(tuple(zip(x_s, y_s)))
+    center_pts = img[x_s, y_s]
+    tol = 0.001
 
-    # Calculate all distances: Iterate until the the centers do not change
-    img_label =
+    while True:
+        # Calculate all the errors
+        print('center points: {}'.format(center_pts))
+        img_errors = np.empty((rows, columns, k))
+        for i in range(rows):
+            for j in range(columns):
+                img_errors[i, j, :] = np.abs(img[i, j]-center_pts)
+
+        # Calculate image labels
+        img_label = np.argmin(img_errors, axis=2)
+
+        # Calculate total error
+        error = np.sum(np.min(img_errors, axis=2))
+        print('error: {}'.format(error))
+        # error = 0
+        # for i in range(rows):
+        #     for j in range(columns):
+        #         error += img_errors[i, j, img_label[i,j]]
+
+        # Calculate new center point (center of the labeled groups)
+        new_center_pts = np.zeros_like(center_pts)
+        counts = np.zeros_like(center_pts, dtype=int)
+        for i in range(rows):
+            for j in range(columns):
+                new_center_pts[img_label[i, j]] += img[i, j]
+                counts[img_label[i, j]] += 1
+
+        # Need to check for divide by zero
+        print(counts)
+        print(new_center_pts)
+        if np.any(counts == 0):
+            break
+        new_center_pts = new_center_pts/counts
+
+        # Iterate until the the centers do not change
+        if np.all(new_center_pts-center_pts < tol):
+            # best_center_pts = new_center_pts
+            # min_error = error
+            # best_img_labels = img_label
+            print(new_center_pts-center_pts)
+            break
+        else:
+            center_pts = new_center_pts
 
     # Iterate on different initializations and output the best center
 
     # return clustering center values, clustering labels of all pixels, the total sum of the centers to each pixel
 
-    return best_pt, best_img_labels, min_error
-    # TODO: implement this function.
+    return new_center_pts, img_label, error
 
 
 def visualize(centers,labels):
@@ -59,13 +105,21 @@ def visualize(centers,labels):
          Clustering labels of all pixels. 
     Return: Segmentation map.
     """
-    # TODO: implement this function.
+    rows, columns = labels.shape
+    im = np.empty_like(labels, dtype=float)
+    for i in range(rows):
+        for j in range(columns):
+            im[i, j] = centers[labels[i, j]]
+
+    print(centers)
+    return im
 
      
 if __name__ == "__main__":
     img = utils.read_image('lenna.png')
+    img = img/255
     k = 2
-
+    print("maximum value: {}".format(np.max(img)))
     start_time = time.time()
     centers, labels, sumdistance = kmeans(img,k)
     result = visualize(centers, labels)
